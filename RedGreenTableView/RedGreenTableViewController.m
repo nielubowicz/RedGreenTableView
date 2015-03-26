9caa262bbc10cb4144f2d71e618f73c2025ce0ec
nielubowicz <nielubowicz@gmail.com>
2015-03-26 01:35:19 -0400

    Adds sets for extra index paths to show and hide expanded cells

--- RedGreenTableView/RedGreenTableViewController.m
+++ RedGreenTableView/RedGreenTableViewController.m
@@ -10,6 +10,8 @@
 
 @interface RedGreenTableViewController ()
 
+@property (strong, nonatomic) NSMutableSet *expandedIndexPaths;
+@property (strong, nonatomic) NSMutableSet *extraIndexPaths;
 @property (strong, nonatomic) NSIndexPath *expandedIndexPath;
 @property (strong, nonatomic) NSIndexPath *extraIndexPath;
 
@@ -17,12 +19,88 @@
 
 @implementation RedGreenTableViewController
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.expandedIndexPaths = [NSMutableSet set];
+    self.extraIndexPaths = [NSMutableSet set];
+}
+
+- (void)addIndexPath:(NSIndexPath *)indexPath toIndexPaths:(NSMutableSet *)indexPathSet
+{
+    NSMutableSet *tempSet = [NSMutableSet setWithObject:indexPath];
+    for (NSIndexPath *knownPath in indexPathSet) {
+        
+        if (knownPath.section == indexPath.section) {
+            if (knownPath.row > indexPath.row) {
+                NSIndexPath *newKnownPath = [NSIndexPath indexPathForRow:knownPath.row + 1 inSection:knownPath.section];
+                [tempSet addObject:newKnownPath];
+            } else {
+                [tempSet addObject:knownPath];
+            }
+        } else {
+            [tempSet addObject:knownPath];
+        }
+    }
+    [indexPathSet setSet:tempSet];
+}
+
+- (void)removeIndexPath:(NSIndexPath *)indexPath fromIndexPaths:(NSMutableSet *)indexPathSet
+{
+    NSMutableSet *tempSet = [NSMutableSet set];
+    for (NSIndexPath *knownPath in indexPathSet) {
+        
+        if (knownPath.section == indexPath.section) {
+            if (knownPath.row > indexPath.row) {
+                NSIndexPath *newKnownPath = [NSIndexPath indexPathForRow:knownPath.row - 1 inSection:knownPath.section];
+                [tempSet addObject:newKnownPath];
+            } else if (knownPath.row < indexPath.row) {
+                [tempSet addObject:knownPath];
+            }
+        } else {
+            [tempSet addObject:knownPath];
+        }
+    }
+    [indexPathSet setSet:tempSet];
+}
+
 #pragma mark - UITableViewDelegate methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    [tableView beginUpdates];
+
+    if ([self.expandedIndexPaths containsObject:indexPath]) {
+        [self removeIndexPath:indexPath fromIndexPaths:self.expandedIndexPaths];
+        self.extraIndexPath = [NSIndexPath indexPathForRow:indexPath.row + 1
+                                                 inSection:indexPath.section];
+
+        [self removeIndexPath:self.extraIndexPath fromIndexPaths:self.extraIndexPaths];
+        [tableView deleteRowsAtIndexPaths: @[ self.extraIndexPath ]
+                         withRowAnimation:UITableViewRowAnimationTop];
+        self.extraIndexPath = nil;
+        
+    } else {
+        [self addIndexPath:indexPath toIndexPaths:self.expandedIndexPaths];
+        self.extraIndexPath = [NSIndexPath indexPathForRow:indexPath.row + 1
+                                                 inSection:indexPath.section];
+        [self addIndexPath:self.extraIndexPath toIndexPaths:self.extraIndexPaths];
+        [tableView insertRowsAtIndexPaths:@[ self.extraIndexPath ]
+                         withRowAnimation:UITableViewRowAnimationBottom];
+    }
+
+    [tableView endUpdates];
+}
+
+
+#pragma mark - UITableViewDatasource methods
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = nil;
-    if ([self.extraIndexPath isEqual:indexPath]) {
+    if ([self.extraIndexPaths containsObject:indexPath]) {
         cell = [tableView dequeueReusableCellWithIdentifier:@"greenCell"];
     } else {
         cell = [tableView dequeueReusableCellWithIdentifier:@"redCell"];
@@ -31,44 +109,22 @@
     return cell;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    self.expandedIndexPath = indexPath;
-    [tableView beginUpdates];
-    if (self.extraIndexPath) {
-        NSIndexPath *tempIndexPath = [self.extraIndexPath copy];
-        self.extraIndexPath = nil;
-        [tableView deleteRowsAtIndexPaths: @[ tempIndexPath ]
-                         withRowAnimation:UITableViewRowAnimationTop];
-    }
+    NSInteger rows = 15;
+    rows += self.extraIndexPaths.count;
     
-    self.extraIndexPath = [NSIndexPath indexPathForRow:self.expandedIndexPath.row + 1
-                                             inSection:self.expandedIndexPath.section];
-    [tableView insertRowsAtIndexPaths:@[ self.extraIndexPath ]
-                     withRowAnimation:UITableViewRowAnimationBottom];
-    [tableView endUpdates];
+    return rows;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     CGFloat height = 44;
-    if ([indexPath isEqual:self.extraIndexPath]) {
+    if ([self.extraIndexPaths containsObject:indexPath]) {
         height = 88;
     }
     
     return height;
-}
-
-#pragma mark - UITableViewDatasource methods
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    NSInteger rows = 15;
-    if (self.extraIndexPath) {
-        rows++;
-    }
-    
-    return rows;
 }
 
 @end
